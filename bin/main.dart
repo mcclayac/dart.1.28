4f472e6b6478218fbce7b3b8b72a9548a0e4fc93
mcclayac <anthony.mcclay@gmail.com>
2020-05-22 14:46:07 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,11 @@
+//code 1.28
+main(List<String> arguments) {
+  var myInteger = {};
+  var mapMyInterger = {};
+  if(myInteger.isEmpty){
+    print("It is a map that has no key, value pair.");
+  } else print("It is a set that has no key, value pair.");
+
+  print("mapMyInteger :" + mapMyInterger.toString());
+
+}
